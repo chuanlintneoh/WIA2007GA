4860e2f13b46466ff8a5d95a74b12a1fa6bc0529
BoonPing <chboonping03@gmail.com>
2024-01-01 18:52:29 +0800

    Submission Fragment and Logic is done.

---
+++ app/src/main/java/com/example/ecoventur/ui/transit/model/Submitted.java
@@ -0,0 +1,84 @@
+package com.example.ecoventur.ui.transit.model;
+
+import android.graphics.Bitmap;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Submitted{
+
+    //instance variables
+    String userID;
+    String challengingID;
+    String challengeTitle;
+    List<String> imageUrl;
+    String description;
+    String ecocoins;
+
+    //Constructor
+    public Submitted() {
+    }
+
+    public Submitted(String userID, String challengingID, String challengeTitle, List<String> imageUrl, String description, String ecocoins) {
+        this.userID = userID;
+        this.challengingID = challengingID;
+        this.challengeTitle = challengeTitle;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.ecocoins = ecocoins;
+    }
+
+    //Getter and Setter
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getChallengingID(){
+        return challengingID;
+    }
+
+    public void setChallengingID(String challengingID){
+        this.challengingID = challengingID;
+    }
+
+    public String getChallengeTitle() {
+        return challengeTitle;
+    }
+
+    public void setChallengeTitle(String challengeTitle) {
+        this.challengeTitle = challengeTitle;
+    }
+
+    public List<String> getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(List<String> imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getEcocoins() {
+        return ecocoins;
+    }
+
+    public void setEcocoins(String ecocoins) {
+        this.ecocoins = ecocoins;
+    }
+
+}

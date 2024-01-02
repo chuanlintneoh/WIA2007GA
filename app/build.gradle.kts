c6968481070e9dc4a9622692aebe6885eff778c6
chuanlintneoh <chuanlin.tneoh@gmail.com>
2024-01-03 00:14:41 +0800

    Merged Module 2: pending for testing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.10.0")
     implementation("com.google.firebase:firebase-storage:20.3.0")
     implementation("com.google.maps:google-maps-services:2.2.0")
+    implementation("com.makeramen:roundedimageview:2.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")

350cbad1393c645ce7b3d493abe082ebedb3da63
chuanlintneoh <chuanlin.tneoh@gmail.com>
2024-01-03 02:24:54 +0800

    UI adjustments

--- app/src/main/java/com/example/ecoventur/MainActivity.java
+++ app/src/main/java/com/example/ecoventur/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_transit, R.id.navigation_greenspace, R.id.navigation_user, R.id.navigation_ecoeducation, R.id.navigation_ecorewards)
+                R.id.navigation_transit, R.id.navigation_greenspace, R.id.navigation_ecoeducation, R.id.navigation_ecorewards, R.id.navigation_user)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);

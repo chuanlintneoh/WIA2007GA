abdc7276221c4d979249832b5ac34365dbee661b
Gan-Serene <116237097+Gan-Serene@users.noreply.github.com>
2024-01-07 13:23:13 +0800

    google login and edit profile picture improvement

--- app/src/main/java/com/example/ecoventur/ui/user/EditProfilePictureActivity.java
+++ app/src/main/java/com/example/ecoventur/ui/user/EditProfilePictureActivity.java
@@ -5,7 +5,6 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
@@ -14,7 +13,6 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -22,15 +20,12 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.ecoventur.R;
-import com.example.ecoventur.ui.greenspace.Callback;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
@@ -41,7 +36,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
-import java.util.UUID;
 
 public class EditProfilePictureActivity extends AppCompatActivity {
     private Uri profilePictureUri = null;
